32ca8effae22eeea9915141db85f77b8b4b02af8
Solenad <roe.dizon80@gmail.com>
2025-03-18 02:38:28 +0800

    fix: fixed mobile responsiveness
    feat: added typewriting anim

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,37 @@
+import { TypeAnimation } from "react-type-animation";
+
 export default function Card() {
   return (
     <>
       <div
-        className="rounded-xl border border-gray-700 shadow-xl overflow-hidden 
+        className="rounded-xl  shadow-xl overflow-hidden 
         flex flex-col
-        w-[90%] h-[85vh] bg-[url(../../../public/images/bg.jpg)] bg-no-repeat bg-cover bg-center"
+        w-[90%] h-[85vh] bg-gradient-to-b from-[var(--card-color1)] to-[var(--card-color2)]"
       >
         <div className="h-full"></div>
         <div className="h-full flex flex-col justify-start text-left px-10 md:px-20 lg:px-40">
-          <h1 className="text-7xl">Roe Dizon</h1>
-          <p className="text-xl"> Backend Developer</p>
+          <p className="text-6xl md:text-7xl lg:text-9xl font-['Montserrat'] mb-4">
+            Roe Dizon
+          </p>
+          <TypeAnimation
+            className="maintitle-landing ml-3"
+            sequence={[
+              "Software Developer",
+              3000,
+              "College Student (2nd year)",
+              3000,
+              "Backend Developer",
+              3000,
+            ]}
+            wrapper="span"
+            speed={20}
+            style={{
+              fontSize: "20px",
+              fontFamily: "JetBrains Mono",
+              display: "block",
+            }}
+            repeat={Infinity}
+          />
         </div>
       </div>
     </>
