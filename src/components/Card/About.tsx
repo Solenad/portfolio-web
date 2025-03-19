807b192d42c01f4f510635a3bc09ed6219aed7b8
Solenad <roe.dizon80@gmail.com>
2025-03-20 02:31:55 +0800

    fix: responsiveness

--- src/components/Card/About.tsx
+++ src/components/Card/About.tsx
@@ -8,7 +8,7 @@ export default function About({ style, sidebarOpen }: props) {
     <div
       className={`${style} flex w-full h-full justify-center md:justify-start lg:justify-start`}
     >
-      <div className="max-h-[60%] flex flex-col lg:flex-row w-full justify-center lg:justify-start items-center gap-25">
+      <div className="max-h-[60%] flex flex-col lg:flex-row w-full justify-center lg:justify-start items-center gap-15 lg:gap-25">
         <div className="bg-[var(--card-block)] max-h-[225px] shadow-lg rounded-lg border border-[var(--card-color1)]">
           <img
             className="w-[225px] p-2"
